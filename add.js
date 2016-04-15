2998148e8a9e74b4bc4156660336f26561dab5cd
Pablo Navarro Castillo <pnavarrc@gmail.com>
2016-04-15 15:58:25 -0300

    add numbers by simple iteration

--- add.js
+++ add.js
@@ -7,10 +7,9 @@ function toInt(num) { return parseInt(num, 10); }
 
 var numbers = args.slice(2, n).map(toInt);
 
-// Adding numbers iterating
-var sum = 0;
-for (var k = 0, m = numbers.length; k < m; k += 1) {
-  sum += numbers[k];
-}
+var sum = numbers.reduce(function(total, num) {
+  return total + num;
+}, 0);
+
 
 console.log(sum);
