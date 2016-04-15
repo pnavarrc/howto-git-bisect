81754db01dc5e69bf3b39263fba907c6c1b337fb
Pablo Navarro Castillo <pnavarrc@gmail.com>
2016-04-15 15:54:24 -0300

    First working version

--- add.js
+++ add.js
@@ -3,7 +3,7 @@
 var args = process.argv,
     n = args.length;
 
-var numbers = args.slice(1, n).map(Number);
+var numbers = args.slice(2, n).map(Number);
 
 var total = numbers.reduce(function(sum, num) {
   return sum + num;
